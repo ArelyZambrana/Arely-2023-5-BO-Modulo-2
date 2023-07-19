cdd5f89736d6cbbeb51b2db8fce1a46d77f6d08b
ArelyZambrana <arelyzam18@gmail.com>
2023-07-19 00:03:54 -0400

    Second-Class

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -17,11 +17,19 @@ class Spaceship(Sprite):
         
     def update(self, user_input):
         if user_input[pygame.K_LEFT]:
-            if self.rect.left > 0:
-                self.rect.x -= 10
+            self.rect.x -= self.SHIP_SPEED
+            if self.rect.left < 0:
+                self.rect.right = SCREEN_WIDTH
         elif user_input[pygame.K_RIGHT]:
-            if self.rect.right < SCREEN_WIDTH:
-                self.rect.x += 10
-        
+            self.rect.x += self.SHIP_SPEED
+            if self.rect.right >= SCREEN_WIDTH:
+                self.rect.left = 0
+        elif user_input[pygame.K_UP]:
+            if self.rect.y < SCREEN_HEIGHT // 2:
+                self.rect.y -= self.SHIP_SPEED
+        elif user_input[pygame.K_DOWN]:
+            if self.rect.y > SCREEN_HEIGHT - self.SPACESHIP_HEIGHT:
+                self.rect.y += self.SHIP_SPEED
+                
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
